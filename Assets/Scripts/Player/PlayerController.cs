7b60bda31e0f168335d4a25c798d6be3af28a62e
Halen Finlay <s210172@students.aie.edu.au>
2023-10-19 15:24:54 +1030

    Basic player controller with shooting completed. Player prefab created. First basic Gun prefab created. Missing most content.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,68 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build.Content;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody rb;
-    
+    // component references
+    private Rigidbody m_rb;
+    private PlayerInput m_playerInput;
+
+    [Header("Default Stats")]
     public float moveSpeed;
     public float maxHealth;
 
-    private float currentHealth;
-    public float fireRate;
-    public float currentAmmo;
+    // player's current stats
+    private float m_currentHealth;
+    private float m_fireRate;
+    private float m_currentAmmo;
+    private Vector3 m_moveForce;
+    private Vector2 m_aimDirection;
 
+    [Header("Starting Gun")]
     public Gun defaultGun;
-    public Gun currentGun;
+    private Gun m_currentGun; // gun the player currently has
+
+    [Header("Aim Indicator")]
+    public GameObject aimIndicatorPrefab;
+    public float aimIndicatorDistance;
+    private GameObject m_aimIndicator; // reference to the indicator after it is instantiated
     
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        m_rb = GetComponent<Rigidbody>();
+        m_playerInput = GetComponent<PlayerInput>();
+        if (!m_aimIndicator)
+        {
+            m_aimIndicator = Instantiate(aimIndicatorPrefab, gameObject.transform);
+        }
+        m_currentGun = defaultGun;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        m_rb.AddForce(m_moveForce, ForceMode.Force); // apply the force to the player
     }
 
     public void OnMove(InputAction.CallbackContext value)
     {
-        rb.AddForce(new Vector3(value.ReadValue<Vector2>().x * moveSpeed, value.ReadValue<Vector2>().y * moveSpeed, 0), ForceMode.Impulse);
+        float inputValue = value.ReadValue<Vector2>().x; // Get the direction the player is holding
+        m_moveForce = moveSpeed * new Vector3(inputValue, 0, 0); // calculate the magnitude of the force
     }
 
     public void OnShoot(InputAction.CallbackContext value)
     {
-
+        if (value.performed)
+        {
+            m_rb.AddForce(m_currentGun.baseRecoil * -m_aimDirection, ForceMode.Impulse);
+        }
     }
 
     public void Aim(InputAction.CallbackContext value)
     {
-
+        m_aimDirection = value.ReadValue<Vector2>();
+        Vector3 indicatorPosition = new Vector3(m_aimDirection.x * aimIndicatorDistance, m_aimDirection.y * aimIndicatorDistance, 0);
+        m_aimIndicator.transform.localPosition = indicatorPosition;
+        m_aimIndicator.transform.rotation = Quaternion.LookRotation(m_aimIndicator.transform.position - m_rb.position);
     }
 
     public void TakeDamage(float damage)
     {
-        currentHealth -= damage;
-        if (currentHealth <= 0) Debug.Log(name + " player is dead.");
+        m_currentHealth -= damage;
+        if (m_currentHealth <= 0) Debug.Log(name + " player is dead.");
+    }
+
+    public void SetControllerMap(string mapName)
+    {
+        
     }
 
     public void EnableInput()
     {
         Debug.Log(name + " player input enabled.");
+        m_playerInput.ActivateInput();
     }
 
     public void DisableInput()
     {
         Debug.Log(name + " player input disabled.");
+        m_playerInput.DeactivateInput();
     }
 
     public void ResetPlayer()
     {
-        currentHealth = maxHealth;
+        m_currentHealth = maxHealth;
 
     }
 }
