6a703752e1de1af9b4960c4848625881d41b6ddd
Halen Finlay <s210172@students.aie.edu.au>
2023-10-25 09:47:36 +1030

    Fixed small issue with Lava script.

--- Assets/Scripts/Objects/Lava.cs
+++ Assets/Scripts/Objects/Lava.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Lava : MonoBehaviour
 {
-    public void OnTriggerEnter(Collision other)
+    public void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
