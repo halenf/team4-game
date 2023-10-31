11771431f4d43b3ccd0363737dba0ffc174adc1a
AIE\s221308 <cameron18556@gmail.com>
2023-11-01 09:33:19 +1030

    added flamethrower

--- Assets/Scripts/Shooting/Guns/MiniGun.cs
+++ Assets/Scripts/Shooting/Guns/MiniGun.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 public class MiniGun : Gun
 {
-    public override void Shoot(int playerID, bool shouldBounce, bool isBig, bool explode)
+    public override void Shoot(int playerID, Bullet.Effect effect)
     {
         transform.parent.gameObject.GetComponent<PlayerController>().Rumble(lowRumbleFrequency, highRumbleFrequency, rumbleTime);
         //find spread rotation change
@@ -15,7 +15,7 @@ public class MiniGun : Gun
 
         // instantiate the bullet
         Bullet bullet = Instantiate(bulletPrefab, bulletSpawnTransform.position, transform.rotation * shootDirection);
-        bullet.Init(playerID, bulletDamage, shouldBounce, bullet.transform.forward * bulletSpeed, isBig, explode);
+        bullet.Init(playerID, bulletDamage, bullet.transform.forward * bulletSpeed, bulletLifeTime, effect);
         // apply recoil to player
         transform.parent.GetComponent<Rigidbody>().AddForce(recoil * -transform.forward, ForceMode.Impulse);
     }
--- Assets/Scripts/Shooting/Guns/Pistol.cs
+++ Assets/Scripts/Shooting/Guns/Pistol.cs
@@ -16,13 +16,13 @@ public class Pistol : Gun
     [Min(0)] public float shootingSpeed;
     
    
-    public override void Shoot(int playerID, bool shouldBounce, bool isBig, bool explode)
+    public override void Shoot(int playerID, Bullet.Effect effect)
     {
-        StartCoroutine(BurstShot(playerID, shouldBounce, isBig, explode));
+        StartCoroutine(BurstShot(playerID, effect));
         
     }
 
-    private IEnumerator BurstShot(int playerID, bool shouldBounce, bool isBig, bool explode)
+    private IEnumerator BurstShot(int playerID, Bullet.Effect effect)
     {
         for (int i = 0; i < burstNumber; i++)
         {
@@ -33,7 +33,7 @@ public class Pistol : Gun
 
             // instantiate the bullet
             Bullet bullet = Instantiate(bulletPrefab, bulletSpawnTransform.position, transform.rotation * shootDirection);
-            bullet.Init(playerID, bulletDamage, shouldBounce, bullet.transform.forward * bulletSpeed, isBig, explode);
+            bullet.Init(playerID, bulletDamage, bullet.transform.forward * bulletSpeed, bulletLifeTime, effect);
             // apply recoil to player
             transform.parent.GetComponent<Rigidbody>().AddForce(recoil * -transform.forward, ForceMode.Impulse);
             // wait for next burst shot
