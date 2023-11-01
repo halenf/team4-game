08141061ca92f12658d0679271ab1c3e855ae84a
Halen Finlay <s210172@students.aie.edu.au>
2023-11-01 10:34:31 +1030

    Camera ScreenShake function completed. Imported Cinemachine package. Began setting up target group for gameplay camera.

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraManager : MonoBehaviour
 {
     public static CameraManager Instance;
+
+    
 
     // Singleton instantiation
     private void Awake()
@@ -22,40 +25,57 @@ public class CameraManager : MonoBehaviour
     // camera reference
     public Camera mainCamera;
 
+    // timer for screen-shake
+    private float m_timer;
+
+    // should the camera be tracking the players
+    private bool m_isTracking;
+
     /// <summary>
     /// Set the position, rotation, and FOV of the camera.
     /// </summary>
     /// <param name="_transform"></param>
     /// <param name="fov"></param>
-    public void SetCameraProperties(Transform _transform, float fov)
-    {
-        SetPosition(_transform);
-        SetFOV(fov);
-    }
-
-    private void SetPosition(Transform _transform)
+    public void SetCameraPosition(Transform _transform)
     {
         transform.position = _transform.position;
         transform.rotation = _transform.rotation;
-    }
-
-    private void SetFOV(float fov)
-    {
-        mainCamera.fieldOfView = fov;
     }
 
     /// <summary>
     /// Cause the camera to shake across the x and y planes for a specified amount of time.
     /// </summary>
     /// <param name="magnitude"></param>
+    /// <param name="intensity"></param>
     /// <param name="time"></param>
-    public void ScreenShake(float magnitude, float time)
+    public void ScreenShake(float magnitude, float intensity, float time)
     {
-        StartCoroutine(StartScreenShake(magnitude, time));
+        StartCoroutine(StartScreenShake(magnitude, intensity, time));
     }
 
-    private IEnumerator StartScreenShake(float magnitude, float time)
+    private IEnumerator StartScreenShake(float magnitude, float intensity, float time)
     {
-        yield return new WaitForSeconds(time);
+        m_timer = time;
+        while (m_timer > 0)
+        {
+            // Get a point for the camera to shake to
+            Vector2 displacement = Random.insideUnitCircle.normalized * magnitude * m_timer / time;
+
+            // shake that thang
+            mainCamera.transform.localPosition = displacement;
+
+            yield return new WaitForSeconds(1f / intensity);
+        }
+
+        // reset camera position
+        mainCamera.transform.localPosition = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        // Always be updating the timer if it is active
+        if (m_timer > 0) m_timer -= Time.deltaTime;
+
+        if (Keyboard.current.digit4Key.isPressed) ScreenShake(3.5f, 5, 1.2f);
     }
 }
