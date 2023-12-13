06a09dfff7a5823c0b5193c4f6538c6a4b2b748e
Halen Finlay <s210172@students.aie.edu.au>
2023-12-13 15:25:39 +1030

    Merged changes.

--- Assets/Scripts/LevelObjects/Obstacles/Laser.cs
+++ Assets/Scripts/LevelObjects/Obstacles/Laser.cs
@@ -13,6 +13,7 @@ public class Laser : Obstacle
     private ParticleSystem m_particleInScene;
     private LineRenderer m_lineRenderer;
     public LayerMask mask;
+    public float killWidth;
     
     public override void Start()
     {
@@ -33,44 +34,51 @@ public class Laser : Obstacle
         if (isActive)
         {
             //do a raycast
-            RaycastHit hit;
+            RaycastHit lineHit;
 
+            // do one ray cast to make the laser and kill players in it
             //if the ray doesnt hit any thing in 1000 units
-            if (Physics.Raycast(transform.position, transform.up, out hit, 1000, mask, QueryTriggerInteraction.Ignore))
+            if (Physics.Raycast(transform.position, transform.up, out lineHit, 1000, mask, QueryTriggerInteraction.Ignore))
             {
                 //draw line between here and end point of ray
                 m_lineRenderer.SetPosition(0, transform.position);
-                m_lineRenderer.SetPosition(1, hit.point);
+                m_lineRenderer.SetPosition(1, lineHit.point);
 
                 // activate the particle system
                 if (!m_particleInScene.isPlaying) m_particleInScene.Play();
 
                 //move particles
                 m_particleInScene.gameObject.SetActive(true);
-                m_particleInScene.transform.position = hit.point;
+                m_particleInScene.transform.position = lineHit.point;
                 m_particleInScene.transform.LookAt(transform.position);
-
-                //if hit a player
-                if (hit.collider.gameObject.tag == "Player")
-                {
-                    //get player
-                    PlayerController hitPlayer = hit.collider.gameObject.GetComponent<PlayerController>();
-                    //do max damage
-                    hitPlayer.TakeDamage(hitPlayer.maxHealth, AnnouncerSubtitleDisplay.AnnouncementType.DeathFire);
-                }
-                if (hit.collider.gameObject.tag == "Breakable")
-                {
-                    //get player
-                    BreakableObject hitObject = hit.collider.gameObject.GetComponent<BreakableObject>();
-                    //do max damage
-                    hitObject.TakeDamage(hitObject.maxHealth);
-                }
             }
             else //just draw line from here to 1000 up
             {
                 m_lineRenderer.SetPosition(0, transform.position);
                 m_lineRenderer.SetPosition(1, transform.up * 1000);
                 m_particleInScene.Stop();
+            }
+
+            RaycastHit boxHit;
+
+            //do box cast so players cant go past the laser
+            if (Physics.BoxCast(transform.position, new Vector3(killWidth / 2f, .5f, .5f), transform.up, out boxHit, Quaternion.identity, 1000))
+            {
+                //if hit a player
+                if (boxHit.collider.gameObject.tag == "Player")
+                {
+                    //get player
+                    PlayerController hitPlayer = boxHit.collider.gameObject.GetComponent<PlayerController>();
+                    //do max damage
+                    hitPlayer.TakeDamage(hitPlayer.maxHealth, AnnouncerSubtitleDisplay.AnnouncementType.DeathFire);
+                }
+                if (boxHit.collider.gameObject.tag == "Breakable")
+                {
+                    //get player
+                    BreakableObject hitObject = boxHit.collider.gameObject.GetComponent<BreakableObject>();
+                    //do max damage
+                    hitObject.TakeDamage(hitObject.maxHealth);
+                }
             }
         }
         else
